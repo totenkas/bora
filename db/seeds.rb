7791d10b21a27ef9b9087babb7e8d0a4988ac0aa
Tomas Vannucchi <tomas.vannucchi@gmail.com>
2024-09-02 20:21:50 -0300

    Created first seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,24 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+puts 'Creating user...'
+
+user1 = User.create!(email: 'tomas@gmail.com', password: '123123')
+user2 = User.create!(email: 'monica@gmail.com', password: '123123')
+
+puts 'Users created!'
+
+puts 'Creating events...'
+
+event1 = Event.create!(
+  user: user2,
+  title: "joana 2 anos",
+  start: DateTime.new(2024, 8, 24, 14, 0, 0),
+  end: DateTime.new(2024, 8, 24, 18, 0, 0),
+  location: "Rua Ernesto Nazaré, 621 - São Paulo",
+  # spots: 50,
+  # cost_per_person: 20.00,
+  # custom_link: "sample-event",
+  description: "Queridos, nossa Jojô está completando dois anos e queremos vocês junto conosco pra comemorar! Será no sábado, 24 de agosto, às 14h aqui em casa."
+  # response_deadline: Date.today + 5.days
+)
+# event1.photo.attach(io: photo1, filename: "paul.png", content_type: "image/png")
+
+puts 'Events created!'
