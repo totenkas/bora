54b2f9c205f61440d9306a22824503b0f0a641ec
Tomas Vannucchi <tomas.vannucchi@gmail.com>
2024-09-03 19:37:36 -0300

    Imported masonry-layout

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,3 +7,9 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "bootstrap", to: "bootstrap.min.js", preload: true
 pin "@popperjs/core", to: "popper.js", preload: true
+pin "masonry-layout" # @4.2.2
+pin "desandro-matches-selector" # @2.0.2
+pin "ev-emitter" # @1.1.1
+pin "fizzy-ui-utils" # @2.0.7
+pin "get-size" # @2.0.3
+pin "outlayer" # @2.1.1
