457b430a2d2caa21be48d67801e51e2f79608ded
Tomas Vannucchi <tomas.vannucchi@gmail.com>
2024-09-07 11:24:52 -0300

    Added images loaded promise to index

--- app/javascript/controllers/masonry_controller.js
+++ app/javascript/controllers/masonry_controller.js
@@ -1,22 +1,34 @@
-import Masonry from 'masonry-layout';
-import imagesLoaded from 'imagesloaded';
-import { Controller } from "@hotwired/stimulus";
+import { Controller } from "@hotwired/stimulus"
+import Masonry from 'masonry-layout'
 
 // Connects to data-controller="masonry"
 export default class extends Controller {
-  static targets = ['image', 'item'];
+  static targets = [ 'image', 'item' ]
 
   connect() {
-    // Initialize Masonry
-    this.masonry = new Masonry(this.element, {
-      itemSelector: ".grid-item",
-      columnWidth: 277,
-      gutter: 30
-    });
+    const imagesLoaded = Promise.all(
+      Array.from(this.imageTargets).map(img => {
+        return new Promise((resolve) => {
+          if (img.complete) {
+            resolve();
+          } else {
+            img.onload = resolve;
+            img.onerror = resolve;
+          }
+        });
+      })
+    );
 
-    // Use imagesLoaded to ensure Masonry layout is applied correctly
-    imagesLoaded(this.element, () => {
-      this.masonry.layout();
+    imagesLoaded.then(() => {
+      console.log('images loaded');
+      this.itemTargets.forEach(item => {
+        item.classList.remove('d-none')
+      });
+      new Masonry( this.element, {
+        "itemSelector": ".grid-item",
+        "columnWidth": 277,
+        "gutter": 30
+      });
     });
   }
 }
